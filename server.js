e5936e0baa312abc369444626353fc0beac71566
Jo Chuang <josephch405@gmail.com>
2017-09-17 03:02:37 -0400

    omg i need sleep

--- server.js
+++ server.js
@@ -139,7 +139,6 @@ app.post("/getChartData", function(req, res) {
         //console.log([sessions.confusion, sessions.distraction])
         res.send(200, [sessions.distraction, sessions.confusion])
     })
->>>>>>> master
 })
 
 
